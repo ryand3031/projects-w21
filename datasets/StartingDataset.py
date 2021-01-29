902f629d971652fe974b1c2230c43dc4ad2e31af
biquando <biquando@gmail.com>
2021-01-28 19:50:09 -0800

    Setup dataset for images

--- datasets/StartingDataset.py
+++ datasets/StartingDataset.py
@@ -1,19 +1,30 @@
 import torch
-
+from PIL import Image
+import torchvision.transforms as transforms
+import pandas as pd
 
 class StartingDataset(torch.utils.data.Dataset):
     """
-    Dataset that contains 100000 3x224x224 black images (all zeros).
+    Dataset that contains 3x224x224 images.
     """
 
     def __init__(self):
-        pass
+        self.transformations = transforms.Compose([
+            transforms.Resize(255),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        # pandas dataframe that stores image names and labels
+        self.df = pd.read_csv('./data/train.csv').sample(frac=1)
 
     def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
+        img = self.df.iloc[index]
+        inputs = self.transformations(Image.open(f"./data/train_images/{img['image_id']}"))
+        label = img['label']
 
         return inputs, label
 
     def __len__(self):
-        return 10000
+        return len(self.df)
